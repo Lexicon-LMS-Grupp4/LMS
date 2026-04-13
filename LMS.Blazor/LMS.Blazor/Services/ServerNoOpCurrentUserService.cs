4ae1b49e013f7c07a3ebd14b0dd54ed9f00c3f56
Felix Havlind <havlindfelix@gmail.com>
2026-04-13 09:49:16 +0200

    Add current user profile support and dynamic nav menu
    Introduce a "Current User" feature with ICurrentUserService and backend API (MeController) to fetch the authenticated user's profile. Update NavMenu to display personalized navigation for students, including dynamic course links and user info. Adjust routing in Modules.razor and register new services for both client and server environments. This enables a more personalized and responsive navigation experience for authenticated users.

---
+++ LMS.Blazor/LMS.Blazor/Services/ServerNoOpCurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using LMS.Blazor.Client.Services;
+using LMS.Shared.DTOs.Common;
+using LMS.Shared.DTOs.Module;
+using LMS.Shared.DTOs.User;
+using Microsoft.Build.Framework;
+
+namespace LMS.Blazor.Services;
+
+public class ServerNoOpCurrentUserService(ILogger<ServerNoOpCurrentUserService> logger) : ICurrentUserService
+{
+    public Task<UserDto?> GetMeAsync(CancellationToken ct = default)
+    {
+        logger.LogWarning("ServerNoOpModuleService called for GetAllModulesAsync");
+        return Task.FromResult<UserDto?>(null);
+    }
+}
